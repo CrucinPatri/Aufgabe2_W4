538cecbc9d6b081fb6589ed8d701e8a3d5da0d44
Aministrator <crucin.patricia@yahoo.com>
2025-10-20 23:04:14 +0300

    Maximum von Array

---
+++ src/FindMaximum.java
@@ -0,0 +1,9 @@
+public class FindMaximum {
+    public int do_max(int[] numbers){
+        int max = numbers[0];
+        for(int i = 1; i<numbers.length; i++)
+            if(numbers[i] > max)
+                max = numbers[i];
+        return max;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {4, 8 , 3, 10, 17};
+
+        FindMaximum find_maximum = new FindMaximum();
+        System.out.println("Maximum: " + find_maximum.do_max(numbers));
+    }
+}
